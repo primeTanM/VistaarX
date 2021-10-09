bd75718b1069f5baa34c978e6dcc26cae0191442
Tanmay <tanmaymisra619@gmail.com>
2021-10-10 04:33:56 +0530

    added avatars

---
+++ src/data/retailers.js
@@ -0,0 +1,72 @@
+export const retailerData = [
+    {
+        id: 1,
+        avatar: "https://img.icons8.com/external-linector-lineal-color-linector/2x/external-avatar-man-avatar-linector-lineal-color-linector-4.png",
+        name: "Michael Scott",
+        company: "Dunder Mifflin",
+        connections: 534
+      },
+      {
+        id: 2,
+        avatar: "https://img.icons8.com/external-linector-lineal-color-linector/2x/external-avatar-man-avatar-linector-lineal-color-linector-5.png",
+        name: "John Cena",
+        company: "WWE",
+        connections: 900
+      },
+      {
+        id: 3,
+        avatar: "https://img.icons8.com/external-linector-lineal-color-linector/2x/external-avatar-man-avatar-linector-lineal-color-linector-7.png",
+        name: "A",
+        company: "ABC",
+        connections: 454
+      },
+      {
+        id: 4,
+        avatar: "https://img.icons8.com/external-linector-lineal-color-linector/2x/external-avatar-man-avatar-linector-lineal-color-linector.png",
+        name: "B",
+        company: "BCD",
+        connections: 349
+      },
+      {
+        id: 5,
+        avatar: "https://img.icons8.com/external-linector-lineal-color-linector/2x/external-avatar-man-avatar-linector-lineal-color-linector-3.png",
+        name: "C",
+        company: "CDE",
+        connections: 923
+      },
+      {
+        id: 6,
+        avatar: "https://img.icons8.com/external-linector-flat-linector/2x/external-avatar-man-avatar-linector-flat-linector-6.png",
+        name: "D",
+        company: "DEF",
+        connections: 934
+      },
+      {
+        id: 7,
+        avatar: "https://img.icons8.com/external-linector-lineal-color-linector/2x/external-avatar-man-avatar-linector-lineal-color-linector-2.png",
+        name: "E",
+        company: "EFG",
+        connections: 234
+      },
+      {
+        id: 8,
+        avatar: "https://img.icons8.com/external-wanicon-lineal-color-wanicon/2x/external-avatar-professions-avatar-wanicon-lineal-color-wanicon-15.png",
+        name: "F",
+        company: "FGH",
+        connections: 345
+      },
+      {
+        id: 9,
+        avatar: "https://img.icons8.com/external-wanicon-flat-wanicon/2x/external-avatar-professions-avatar-wanicon-flat-wanicon-15.png",
+        name: "G",
+        company: "GHI",
+        connections: 453
+      },
+      {
+        id: 10,
+        avatar: "https://img.icons8.com/external-wanicon-lineal-color-wanicon/2x/external-avatar-professions-avatar-wanicon-lineal-color-wanicon-17.png",
+        name: "H",
+        company: "HIJ",
+        connections: 745
+      }
+]

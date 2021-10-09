7ba81268a1bea7eef6c0b607a461566678464f66
Tanmay <tanmaymisra619@gmail.com>
2021-10-10 01:37:17 +0530

    added avatars

--- src/App.js
+++ src/App.js
@@ -6,60 +6,70 @@ function App() {
   const [cards, setCard] = useState([
     {
       id: 1,
+      avatar: "./avatar/avataaars.png",
       name: "Michael Scott",
       company: "Dunder Mifflin",
       connections: 534
     },
     {
       id: 2,
+      avatar: "./avatar/avataaars(1).png",
       name: "John Cena",
       company: "WWE",
       connections: 900
     },
     {
       id: 3,
+      avatar: "./avatar/avataaars(2).png",
       name: "A",
       company: "ABC",
       connections: 454
     },
     {
       id: 4,
+      avatar: "./avatar/avataaars(3).png",
       name: "B",
       company: "BCD",
       connections: 349
     },
     {
       id: 5,
+      avatar: "./avatar/avataaars(4).png",
       name: "C",
       company: "CDE",
       connections: 923
     },
     {
       id: 6,
+      avatar: "./avatar/avataaars(5).png",
       name: "D",
       company: "DEF",
       connections: 934
     },
     {
       id: 7,
+      avatar: "./avatar/avataaars(6).png",
       name: "E",
       company: "EFG",
       connections: 234
     },
     {
       id: 8,
+      avatar: "./avatar/avataaars(7).png",
       name: "F",
       company: "FGH",
       connections: 345
     },
     {
       id: 9,
+      avatar: "./avatar/avataaars(8).png",
       name: "G",
       company: "GHI",
       connections: 453
     },
     {
       id: 10,
+      avatar: "./avatar/avataaars(9).png",
       name: "H",
       company: "HIJ",
       connections: 745
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,9 +3,8 @@ import { FaTimesCircle } from "react-icons/fa"
 
 const Card = ({ card, onDelete }) => {
     return (
-        <div>
-           <FaTimesCircle 
-                onClick={() => onDelete(card.id) } />
+        <div className='card'>
+           <FaTimesCircle onClick={() => onDelete(card.id) } />
            <h1>{ card.name }</h1>
            <h3>{ card.company }</h3>
            <div className='connections'>

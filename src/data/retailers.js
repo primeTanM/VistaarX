4bf60ce9c33cbf72fa45d33577cc6cb2961e839b
Tanmay <tanmaymisra619@gmail.com>
2021-10-10 23:06:57 +0530

    updated README

--- src/data/retailers.js
+++ src/data/retailers.js
@@ -2,71 +2,71 @@ export const retailerData = [
     {
         id: 1,
         avatar: "https://img.icons8.com/external-linector-lineal-color-linector/2x/external-avatar-man-avatar-linector-lineal-color-linector-4.png",
-        name: "Michael Scott",
+        name: "Dave Thomas",
         company: "Dunder Mifflin",
         connections: 534
       },
       {
         id: 2,
         avatar: "https://img.icons8.com/external-linector-lineal-color-linector/2x/external-avatar-man-avatar-linector-lineal-color-linector-5.png",
-        name: "John Cena",
-        company: "WWE",
+        name: "Sean Paul",
+        company: "JFK Tech",
         connections: 900
       },
       {
         id: 3,
         avatar: "https://img.icons8.com/external-linector-lineal-color-linector/2x/external-avatar-man-avatar-linector-lineal-color-linector-7.png",
-        name: "A",
-        company: "ABC",
+        name: "Núria Roddy",
+        company: "Developer Leads",
         connections: 454
       },
       {
         id: 4,
         avatar: "https://img.icons8.com/external-linector-lineal-color-linector/2x/external-avatar-man-avatar-linector-lineal-color-linector.png",
-        name: "B",
-        company: "BCD",
+        name: "Elliot Denice",
+        company: "Axion Tech",
         connections: 349
       },
       {
         id: 5,
         avatar: "https://img.icons8.com/external-linector-lineal-color-linector/2x/external-avatar-man-avatar-linector-lineal-color-linector-3.png",
-        name: "C",
-        company: "CDE",
+        name: "John Doe",
+        company: "Oscort Pvt",
         connections: 923
       },
       {
         id: 6,
         avatar: "https://img.icons8.com/external-linector-lineal-color-linector/2x/external-avatar-man-avatar-linector-lineal-color-linector-6.png",
-        name: "D",
-        company: "DEF",
+        name: "Danielle Radbluffie",
+        company: "Wizardy Ltd",
         connections: 934
       },
       {
         id: 7,
         avatar: "https://img.icons8.com/external-linector-lineal-color-linector/2x/external-avatar-man-avatar-linector-lineal-color-linector-2.png",
-        name: "E",
-        company: "EFG",
+        name: "Buddy Murphy",
+        company: "WWE",
         connections: 234
       },
       {
         id: 8,
         avatar: "https://img.icons8.com/external-linector-lineal-color-linector/2x/external-avatar-man-avatar-linector-lineal-color-linector-1.png",
-        name: "F",
-        company: "FGH",
+        name: "Jervis Amity",
+        company: "UI Phoenix",
         connections: 345
       },
       {
         id: 9,
         avatar: "https://img.icons8.com/plasticine/2x/rick-sanchez.png",
-        name: "G",
-        company: "GHI",
+        name: "Rick",
+        company: "Rick&Morty",
         connections: 453
       },
       {
         id: 10,
         avatar: "https://img.icons8.com/external-ddara-lineal-color-ddara/2x/external-imam-religious-leader-muslim-islam-character-user-avatar-islam-ddara-lineal-color-ddara.png",
-        name: "H",
-        company: "HIJ",
+        name: "Abdul",
+        company: "Client Core",
         connections: 745
       }
 ]
